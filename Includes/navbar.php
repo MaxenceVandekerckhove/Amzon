a97841ff92ac00c30f8042dd49e091c5f7ab8a62
Maxence Vdk <maxence.vandekerckhove@viacesi.fr>
2021-02-12 11:45:58 +0100

    fichier d'includes

---
+++ Includes/navbar.php
@@ -0,0 +1,51 @@
+<nav>
+
+			<ul>
+
+				<div id="logo-click">
+					<a href="index.php" id="logo"><img src="Image/logo.png"></a>
+				</div>
+	
+				<li class="menu-organisation">
+				<a href="">Acheter un article</a>
+
+					<ul class="submenu">
+						<li><a href="">Nouveautés</a></li>
+						<li><a href="">Les plus achetés</a></li>
+						<li><a href="">Sélectionné pour vous</a></li>
+						<li><a href="">Ventes flashs</a></li>
+					</ul>
+
+				</li>
+
+				<li class="menu-banque">
+				<a href="">Vendre un article</a>
+
+					<ul class="submenu">
+						<li><a href="">Déposer vos articles</a></li>
+						<li><a href="">Guide de vente</a></li>
+						<li><a href="">Vos avis</a></li>
+					</ul>
+
+				</li>
+
+				<li class="menu-contact">
+				<a href="">Contact</a>
+				
+					<ul class="submenu">
+						<li><a href="">Contactez-Nous</a></li>
+						<li><a href="">Service Client</a></li>
+						<li><a href="">Liens</a></li>
+					</ul>
+
+				</li>
+
+
+			</ul>
+
+	</nav>
+
+		<div id=login>
+			<a href="login.php">Connexion</a>
+			<a href="register.php">S'enregister</a>
+		</div>
