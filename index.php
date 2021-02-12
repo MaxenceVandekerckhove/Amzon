c145fcda1f0f76528de00ce27609494f2309258e
Maxence Vdk <maxence.vandekerckhove@viacesi.fr>
2021-02-12 11:43:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style_index.css">
+	<title> Amzon - Le meilleur site de e-commerce </title>
+
+</head>
+
+<body>
+
+<?php include("Includes/navbar.php") ?>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="produit">
+
+	<div class ="image_produit">
+
+		<img src="Image/haricots.jpg">
+
+	</div>
+
+	<div class ="text_produit">
+
+
+		<h2>Produit Numéro 1</h2>
+
+		<br/>
+
+		<h4>Description du produit :</h4>
+
+		<br/>
+
+		<p>Eodem tempore etiam Hymetii praeclarae indolis viri negotium est actitatum, cuius hunc novimus esse textum. cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex horreis Romano populo destinatis frumentum dedit, pauloque postea cum provenisset segetum copia, integre sine ulla restituit mora.</p>
+		
+	</div>
+
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="produit">
+
+	<div class ="image_produit">
+
+		<img style="width: 120%;" src="Image/cercueil.jpeg">
+
+	</div>
+
+	<div class ="text_produit">
+
+
+		<h2>Produit Numéro 2</h2>
+
+		<br/>
+
+		<h4>Description du produit :</h4>
+
+		<br/>
+
+		<p>Eodem tempore etiam Hymetii praeclarae indolis viri negotium est actitatum, cuius hunc novimus esse textum. cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex horreis Romano populo destinatis frumentum dedit, pauloque postea cum provenisset segetum copia, integre sine ulla restituit mora.</p>
+		
+	</div>
+
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="produit">
+
+	<div class ="image_produit">
+
+		<img style="width: 120%;" src="Image/abri.jpg">
+
+	</div>
+
+	<div class ="text_produit">
+
+
+		<h2>Produit Numéro 3</h2>
+
+		<br/>
+
+		<h4>Description du produit :</h4>
+
+		<br/>
+
+		<p>Eodem tempore etiam Hymetii praeclarae indolis viri negotium est actitatum, cuius hunc novimus esse textum. cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex horreis Romano populo destinatis frumentum dedit, pauloque postea cum provenisset segetum copia, integre sine ulla restituit mora.</p>
+		
+	</div>
+
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="produit">
+
+	<div class ="image_produit">
+
+		<img style="width: 120%;" src="Image/voiture.jpg">
+
+	</div>
+
+	<div class ="text_produit">
+
+
+		<h2>Produit Numéro 4</h2>
+
+		<br/>
+
+		<h4>Description du produit :</h4>
+
+		<br/>
+
+		<p>Eodem tempore etiam Hymetii praeclarae indolis viri negotium est actitatum, cuius hunc novimus esse textum. cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex horreis Romano populo destinatis frumentum dedit, pauloque postea cum provenisset segetum copia, integre sine ulla restituit mora.</p>
+		
+	</div>
+
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+</body>
+
+</html>
